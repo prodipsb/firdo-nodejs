bc1f0746fadc655c2300eea802fbd085d126131e
prodipsb <prodip5080@gmail.com>
2023-12-12 22:38:36 +0600

    port change 5000 to 8000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const cors = require('cors')
 
-const PORT =process.env.PORT || 5000;
+const PORT =process.env.PORT ||8000;
 const {mogoUrl} = require('./keys')
 
 const bodyParser = require('body-parser')
